159a2fcc98728d31e0c60cf8084be8cbe35f4d44
rajendra0968jangid <rajendra0968jangid@gmail.com>
2026-01-13 12:55:16 +0530

    day2

--- day2.js
+++ day2.js
@@ -13,7 +13,7 @@
 
 // const obj = {age:20};
 // obj["age"] = 22;
-// console.log(obj);
+// console.log(obj["age"]);
 
 //&& || !
 // let num1 = []; // blank array and object are true
@@ -26,13 +26,50 @@
 
 
 
-const newArray = [];
-if(newArray.length){
-    console.log("true is work");
-}
-else{
-    console.log("false is work");
-}
+// const newArray = [];//true
+// if(0 && 0){ //false
+//     console.log("true is work");
+// }
+// else{
+//     console.log("false is work");
+// }
+
+// console.log(0 && 0);
+// console.log(!!10 || "hello")
+
+// let ch = 10;
+// switch(ch){
+//     case "10":
+//         console.log("10");
+//         break;
+//     case true:
+//         console.log("true");
+//         break;
+//     case 10:
+//         console.log("10 in string");
+//         break;
+// }
+
+// let num1 = 10;
+// let num2 = "10";
+// console.log(typeof num1) //number
+// console.log(typeof num2) //string
+// console.log(num1 === num2); //=== => value and type
+// console.log(num1 !== num2); //== => value 
+
+//ternary opr.
+let num1 = 100;
+let num2 = 200;
+let num3 = 300;
+// console.log(num1>num2?num1:num2);
+console.log(num1>num2 && num1 > num3 ?"num1":num2>num3?"num2":"num3");
+
+
+
+
+
+
+
 
 
 
