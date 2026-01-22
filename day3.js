f849c5a77749f6c5aa92a93bff497db6315acc6c
rajendra0968jangid <rajendra0968jangid@gmail.com>
2026-01-22 10:40:40 +0530

    test

---
+++ day3.js
@@ -0,0 +1,43 @@
+//if else || ternary opr
+// 18 == 18 < => vote
+//not vote
+// let age = 20;
+// if(age >= 18){
+//     console.log("vote")
+// }
+// else{
+//     console.log("not vote")
+// }
+
+
+// []=> user absent
+// []=> user present
+// array => user present || user not present
+// [] || [1,2,3] => true
+// {} || {key:value} => true
+// const newArray = [1];
+// if(newArray.length){ //
+//     console.log("user present");
+// }
+// else{
+//     console.log("user absent");
+// }
+
+// const newArray = [1,2,3];
+// console.log(newArray.length?" present":"not present");
+
+// use two num1,num2 => 
+    // let num1 = 10;
+// let num2 = 20;
+// if(num2 > num1){
+    //     console.log(num2)
+// }
+// else{
+    //     console.log(num1)
+    // }
+
+
+// user three num1,num2,num3 => 
+//lucky, laksh,pretham,abu saywed
+
+
---
+++ day4.js
@@ -0,0 +1,55 @@
+// let name = "your-name"
+// console.log(name)
+
+
+// var -> global 
+//let ,const -> block scope
+// var age = 20;
+// console.log(age);
+
+// {
+//     var age = 30;
+//     console.log(age)
+// }
+
+// console.log(age)
+
+
+// //1 row
+// {
+//  const  age = 20;
+//  console.log(age)
+// }
+// {
+//  const  age = 30;
+//  console.log(age)
+// }
+// {
+//  const  age = 40;
+//  console.log(age)
+// }
+// console.log(age)
+
+
+// let age = 20;
+// {
+//     var age = 30;
+//     {
+//         var age = 40;
+//         console.log(age)
+//     }
+// }
+// console.log(age);
+
+// var age = 20;
+// var age = 30;
+// console.log(age);
+
+// let age = 20;
+// let age = 30;
+// console.log(age)
+
+// const age = 20;
+// const age = 30;
+// console.log(age)
+
